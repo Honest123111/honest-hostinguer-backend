906dcffa20528f60ca74456fb6ff72b925e4c110
Daniela Pinzon <danispc389@gmail.com>
2025-02-04 22:22:07 -0500

    Configurado PostgreSQL en Heroku

--- honestdb_project/settings.py
+++ honestdb_project/settings.py
@@ -101,12 +101,10 @@ WSGI_APPLICATION = 'honestdb_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL')
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -203,6 +201,7 @@ SESSION_CACHE_ALIAS = "default"
 PROGRESS_PICTURES_URL = '/progress_pictures/'
 PROGRESS_PICTURES_ROOT = BASE_DIR / 'progress_pictures'
 
+import dj_database_url
 import django_heroku
 django_heroku.settings(locals())
 
